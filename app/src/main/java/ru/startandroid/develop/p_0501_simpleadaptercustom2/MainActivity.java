b7447302461174fa89377089b678823a372f6ae7
VictorTsenner <burzum2007@ya.ru>
2020-12-10 15:22:53 +0300

    'test'

---
+++ app/src/main/java/ru/startandroid/develop/p_0501_simpleadaptercustom2/MainActivity.java
@@ -0,0 +1,97 @@
+package ru.startandroid.develop.p_0501_simpleadaptercustom2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //имена атрибутов для Map
+    final String ATRIBUTE_NAME_TEXT = "text";
+    final String ATRIBUTE_NAME_PB = "pb";
+    final String ATRIBUTE_NAME_LL = "ll";
+
+    ListView lvSipmle;
+    /**Called then activity is first created**/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //массив данных
+
+        int[] load = {41, 48, 61, 22, 76, 98, 45, 23 };
+
+        //упаковываем данные в понятную для адаптера структуру
+        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(load.length);
+        Map<String, Object> m;
+        for (int i = 0; i<load.length; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATRIBUTE_NAME_TEXT, "Day" + (i+1) + " . Load: " + load[i] + "%");
+            m.put(ATRIBUTE_NAME_PB,load[i]);
+            m.put(ATRIBUTE_NAME_LL,load[i]);
+            data.add(m);
+        }
+
+        //создаем массив имен атрибутов, из которых будут читаться данные
+        String[] from = {ATRIBUTE_NAME_TEXT, ATRIBUTE_NAME_PB, ATRIBUTE_NAME_LL};
+
+        //массив ID View компонентов, в которые будут вставляться данные
+        int[] to = {R.id.tvLoad, R.id.pbLoad, R.id.llLoad};
+
+        //создаем адаптер
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        //указываем адаптеру свой биндер
+        sAdapter.setViewBinder(new MyViewBinder());
+
+        //определяем класс и присваиваем ему свой биндер
+        lvSipmle = (ListView) findViewById(R.id.lvSimple);
+        lvSipmle.setAdapter(sAdapter);
+
+    }
+    class MyViewBinder implements SimpleAdapter.ViewBinder {
+
+        int red = getResources().getColor(R.color.Red);
+        int orange = getResources().getColor(R.color.Orange);
+        int green = getResources().getColor(R.color.Green);
+
+        @Override
+        public boolean setViewValue(View view, Object data, String textRepresentation) {
+            int i = 0;
+            switch (view.getId()) {
+                case R.id.llLoad:
+                    i = ((Integer) data).intValue();
+                    if (i < 40) {
+                        view.setBackgroundColor(green);
+                    } else if (i < 70) view.setBackgroundColor(orange);
+                    else
+                        view.setBackgroundColor(red);
+                    return true;
+                case R.id.pbLoad:
+                    i = ((Integer) data).intValue();
+                    ((ProgressBar) view).setProgress(i);
+                    return true;
+            }
+
+
+            return false;
+        }
+    }
+}
